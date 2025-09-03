7c36eef92b468ea08d1dd65778b20ae52b9c8bb2
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-03 11:14:34 -0300

    feat: atividade 2

---
+++ atividade 2/atividade 2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("digite seu número aqui: ");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine($"o antecessor é: {num - 1}");

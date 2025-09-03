ac8945a7b4739769d64144ee7f98964257da6e5d
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-03 12:13:28 -0300

    feat: atividade 5

---
+++ atividade 5/atividade 5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int nvb;
+int nvn;
+int nvv;
+Console.WriteLine("digite o número de votos brancos aqui: ");
+nvb = int.Parse(Console.ReadLine());
+Console.WriteLine($"a porcenagem é: {nvb/100} %");
+Console.WriteLine("digite o número de votos nulos aqui: ");
+nvn = int.Parse(Console.ReadLine());
+Console.WriteLine($"a porcenagem é: {nvn / 100} %");
+Console.WriteLine("digite o número de votos válidos aqui: ");
+nvv = int.Parse(Console.ReadLine());
+Console.WriteLine($"a porcenagem é: {nvv / 100} %");

ad444b9f142eb45f2cb2e3b5c0ce97252ec4621c
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 10:52:17 -0300

    feat: atividade extra 1

---
+++ extra 1/extra 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double n;
+Console.WriteLine("QUANTOS REAIS VOCÊ TEM NA CARTEIRA: ");
+n = Double.Parse(Console.ReadLine());
+Console.WriteLine($"QUANTOS DOLARES VOCÊ TEM NA CARTEIRA: {n / 3.45}");

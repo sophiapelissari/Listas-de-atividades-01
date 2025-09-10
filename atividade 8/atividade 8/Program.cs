c8bad49484f60231edb6a10f4fbf25ccd19909c0
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 09:00:17 -0300

    feat: atividade 8

---
+++ atividade 8/atividade 8/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+int n;
+Console.WriteLine("DIGITE SEU VALOR PARA DESCOBRIR SE É POSITIVO OU NEGATIVO: ");
+n = int.Parse(Console.ReadLine());
+if (n == 0)
+{
+   Console.WriteLine("VALOR INVÁLIDO!");
+
+}
+else if (n >0)
+{
+   Console.WriteLine("SEU NÚMERO É POSITIVO!");
+}
+else
+{
+    Console.WriteLine("SEU NÚMERO É NEGATIVO!");
+}

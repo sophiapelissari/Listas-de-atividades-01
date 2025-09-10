fead8077bb485d01c2c6c084b744f9eff002e89e
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 11:01:40 -0300

    feat: atividade extra 4

---
+++ extra 4/extra 4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double n;
+Console.WriteLine("DIGITE O VALOR DO PRODUTO DESEJADO: ");
+n = double.Parse(Console.ReadLine());
+Console.WriteLine($"O VALOR FINAL DO PRODUTO DESEJADO É: {n * 0.95}");

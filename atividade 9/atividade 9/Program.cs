3229b800cc81f1df7f9161e85deb6977c83ec2cf
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 09:11:45 -0300

    feat: atividade 9

---
+++ atividade 9/atividade 9/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n;
+Console.WriteLine("DIGITE A QUANTIDADE DE MAÇÃS COMPRADAS: ");
+n = int.Parse(Console.ReadLine());
+if(n > 12)
+{
+    Console.WriteLine($"O VALOR A PAGAR É: { n * 1}");
+}
+else
+{
+    Console.WriteLine($"O VALOR A PAGAR É: {n * 1.30}");
+}

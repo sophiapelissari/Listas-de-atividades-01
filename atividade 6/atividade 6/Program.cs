a46ac8ea1fecccce86bc94e228d997482cc66a8a
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 08:31:14 -0300

    feat: atividade 6

---
+++ atividade 6/atividade 6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int salario;
+float reajuste;
+
+Console.WriteLine("Digite o valor do seu salário: ");
+salario = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor do seu reajuste: ");
+reajuste = float.Parse(Console.ReadLine());
+Console.WriteLine($"o valor do seu novo salário é: {(reajuste / 100 * salario) + salario} ");

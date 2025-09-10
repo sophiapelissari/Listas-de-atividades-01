7fac75c8ea8c30c204e1ef241c2e72ece27bedd9
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 08:48:39 -0300

    feat: atividade 7

---
+++ atividade 7/atividade 7/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n;
+Console.WriteLine("DIGITE UM NÚMERO: ");
+n = int.Parse(Console.ReadLine());
+if (n > 10)
+{
+    Console.WriteLine("SEU NÚMERO É MAIOR QUE 10");
+}
+else
+{
+    Console.WriteLine("SEU NÚMERO É MENOR QUE 10");
+}

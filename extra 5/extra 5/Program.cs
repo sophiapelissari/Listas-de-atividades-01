9c4ff70a0f482c9e10aeacb3bae5c9a3cadf0ee8
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-10 11:15:58 -0300

    feat: atividade extra 5

---
+++ extra 5/extra 5/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double n;
+Console.WriteLine("QUAL O SEU SALÁRIO ATUAL: ");
+n = double.Parse(Console.ReadLine());
+Console.WriteLine($"O SEU NOVO SALÁRIO É: {n * 1.15}");

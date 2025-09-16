7493254ae7e0ae1204fbd8bfd4cb616f4c16a83d
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-16 08:35:05 -0300

    feat: atividade 14

---
+++ atividade 14/atividade 14/Program.cs
@@ -0,0 +1,9 @@
+﻿int num;
+Console.WriteLine("digite um número aqui: ");
+num = int.Parse(Console.ReadLine());
+
+for (int contador = 0; contador <= num; contador++)
+{
+    Console.WriteLine(contador);
+    
+}

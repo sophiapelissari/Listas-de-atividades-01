369b8e3d0b6d6e5fee82de4a1897fe58fcbf532a
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-16 08:56:55 -0300

    feat: atividade 15

---
+++ atividade 15/atividade 15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("números entre 1000 e 2000, que dividido por 11, resta 5: ");
+
+    for (int i = 1000; i <= 2000; i++)
+    {
+        if (i % 11 == 5)
+        {
+            Console.WriteLine(i);
+        }
+    }

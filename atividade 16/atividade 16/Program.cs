3c2be58537d445531f4bd0be10bdde45a62fd64a
sophia pelissari <sophiasecundaria04@gmail.com>
2025-09-16 09:21:34 -0300

    feat: atividade 16

---
+++ atividade 16/atividade 16/Program.cs
@@ -0,0 +1,22 @@
+﻿int num;
+Console.WriteLine("digite um número inteiro para descobrir o valor de seu fatorial: ");
+num = int.Parse(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine("NEGATIVO, NÃO EXISTE");
+}
+else if (num == 0)
+{
+    Console.WriteLine("POR DEINIÇÃO IGUAL A 1");
+}
+else
+{
+    int contador = 1;
+    int resultado = 1;
+    while (contador <= num)
+    {
+        resultado *= contador;  
+        contador++;
+    }
+    Console.WriteLine(resultado);
+}
